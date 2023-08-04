d259532972c3ea92740f39c9b24d400a6d387f26
testtesttesttesttesttestaaaa <103189094+testtesttesttesttesttestaaaa@users.noreply.github.com>
2023-08-04 04:47:10 -0700

    Update cli.py

--- poulstar/poulstar/cli.py
+++ poulstar/poulstar/cli.py
@@ -6,49 +6,49 @@ from typing_extensions import Annotated
 app = typer.Typer(pretty_exceptions_show_locals=False)
 
 
-@app.command(help="Export your lists.")
+@app.command(help="Put your logo on the images.")
 def logo(
-    output: Annotated[Path, typer.Argument()] = "output_image.png",
-    background: Path = typer.Option(...),
-    overlay: Path = typer.Option(...),
+    # output: Annotated[Path, typer.Argument()] = "output_image.png",
+    # background: Path = typer.Option(...),
+    # overlay: Path = typer.Option(...),
 ):
-    if background.exists():
-        pass
-    else:
-        print("background image does not exist.")
-        exit()
-    if overlay.exists():
-        pass
-    else:
-        print("Overlay image does not exist.")
-        exit()
+    # if background.exists():
+    #     pass
+    # else:
+    #     print("background image does not exist.")
+    #     exit()
+    # if overlay.exists():
+    #     pass
+    # else:
+    #     print("Overlay image does not exist.")
+    #     exit()
     
-    if not output.parent.exists():
-        print("Your path does not exist.")
-        exit()
-    elif output.parent.exists():
-        if output.suffix == ".png":
-            pass
-        else:
-            print("Invalid file extension. Only .png files are allowed.")
-            exit()
+    # if not output.parent.exists():
+    #     print("Your path does not exist.")
+    #     exit()
+    # elif output.parent.exists():
+    #     if output.suffix == ".png":
+    #         pass
+    #     else:
+    #         print("Invalid file extension. Only .png files are allowed.")
+    #         exit()
 
-    pos = input(
-    """Enter Your Position: 
-    rt = right/top
-    lt = left/top
-    rb = right/bottom
-    lb = left/bottom
-    ct = center/top
-    cb = center/bottom
-    cm = center/middle
-    """
-    )
-    logo.merge_images(
-        background_path=background,
-        overlay_path=overlay,
-        output_path=output,
-        position=pos,
+    # pos = input(
+    # """Enter Your Position: 
+    # rt = right/top
+    # lt = left/top
+    # rb = right/bottom
+    # lb = left/bottom
+    # ct = center/top
+    # cb = center/bottom
+    # cm = center/middle
+    # """
+    # )
+    merge_images(
+        background_path="./1.png",
+        overlay_path="./logo.png",
+        output_path="output.png",
+        position="lt",
     )
 
 
